5cba669eb9aed7b817570bb307a4351318fb19f9
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-29 15:13:39 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+echo "# enventoryManagement" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alnooristiak/enventoryManagement.git
+git push -u origin main
